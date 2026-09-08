d82a3df1968b8690f43ed6199c9078cefac3054a
sandz36 <sandile.mdluli@umuzi.org>
2026-09-08 09:01:14 -0400

    remove whitespace

--- service/routes.py
+++ service/routes.py
@@ -70,10 +70,10 @@ def create_accounts():
 
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def get_accounts(account_id): 
-    """Reads an Account
-    This endpoint will read an Account based the account_id that is requested
-    """
+def get_accounts(account_id):
+    # """Reads an Account
+    # This endpoint will read an Account based the account_id that is requested
+    # """
     app.logger.info("Request to read an Account with id: %s", account_id)
 
     # use the Account.find() method to find the account
@@ -83,9 +83,9 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
         # return the serialize() version of the account with a return code of status.HTTP_200_OK
 
-    return account.serialize(), status.HTTP_200_OK       
+    return account.serialize(), status.HTTP_200_OK    
 
-    
+  
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
